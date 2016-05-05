691645c651fb67546957697d81392ffcc1959e29
Sean Hooper <Sean Hooper>
2016-05-05 11:24:22 +0100

    borders

---
+++ app/src/main/java/com/zendo/zzz/AdResultActivity.java
@@ -0,0 +1,17 @@
+package com.zendo.zzz;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Spinner;
+
+/**
+ * Created by Sean on 05/05/2016.
+ */
+public class AdResultActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ad_results);
+
+    }
+}
